759914e24b57e44f742470d57a3777c5ef29666b
Michel <michelmanthey@gmx.de>
2013-02-14 13:01:27 +0100

    Argumentabfrage eingebaut

--- src/ClassifyNews.java
+++ src/ClassifyNews.java
@@ -1,33 +1,26 @@
 
-import com.aliasi.classify.Classification;
-import com.aliasi.classify.Classified;
-import com.aliasi.classify.ConfusionMatrix;
-import com.aliasi.classify.DynamicLMClassifier;
-import com.aliasi.classify.JointClassification;
-import com.aliasi.classify.JointClassifier;
-import com.aliasi.classify.JointClassifierEvaluator;
-import com.aliasi.classify.LMClassifier;
-
+import com.aliasi.classify.*;
 import com.aliasi.lm.NGramProcessLM;
-
 import com.aliasi.util.AbstractExternalizable;
-
-import java.io.File;
-import java.io.IOException;
-
 import com.aliasi.util.Files;
+
+import java.io.*;
 
 public class ClassifyNews {
 
-	private static File TRAINING_DIR
+	private static File[] TRAINING_DIR
 			//= new File("../../data/fourNewsGroups/4news-train");
 	        //= new File("C:\\Users\\Arne\\Developing\\lib\\lingpipe-4.1.0\\demos\\data\\fourNewsGroups - Kopie\\4news-train");
-	        = new File("C:\\Users\\Arne\\Developing\\Text_Analytics\\A6_Lingpipe\\mailData\\train");
+	        = {new File("C:\\Studium Informatik\\11.FS\\Text Analytics\\A6_SpamFilter\\mailData\\train"),
+			new File("C:\\Studium Informatik\\11.FS\\Text Analytics\\A6_SpamFilter\\mailData\\train")};
+
+	private static File TRAIN_HAM = new File("");
+	private static File TRAIN_SPAM = new File("");
 
 	private static File TESTING_DIR
 			//=  new File("../../data/fourNewsGroups/4news-test");
 			//=  new File("C:\\Users\\Arne\\Developing\\lib\\lingpipe-4.1.0\\demos\\data\\fourNewsGroups - Kopie\\4news-test");
-			= new File("C:\\Users\\Arne\\Developing\\Text_Analytics\\A6_Lingpipe\\mailData\\test");
+			= new File("C:\\Studium Informatik\\11.FS\\Text Analytics\\A6_SpamFilter\\mailData\\test");
 
 	private static String[] CATEGORIES
 			/*= { "soc.religion.christian",
@@ -42,69 +35,83 @@ public class ClassifyNews {
 			throws ClassNotFoundException, IOException {
 
 		DynamicLMClassifier<NGramProcessLM> classifier
-				= DynamicLMClassifier.createNGramProcess(CATEGORIES,NGRAM_SIZE);
+				= DynamicLMClassifier.createNGramProcess(CATEGORIES, NGRAM_SIZE);
 
-		for(int i=0; i<CATEGORIES.length; ++i) {
-			File classDir = new File(TRAINING_DIR,CATEGORIES[i]);
-			if (!classDir.isDirectory()) {
-				String msg = "Could not find training directory="
-						+ classDir
-						+ "\nHave you unpacked 4 newsgroups?";
-				System.out.println(msg); // in case exception gets lost in shell
-				throw new IllegalArgumentException(msg);
+		if (args[0].equals("learn"))
+		{
+			TRAINING_DIR[1] = new File(args[2]);
+			TRAINING_DIR[0] = new File(args[1]);
+
+			for(int i = 0; i < CATEGORIES.length; ++i)
+			{
+				File classDir = TRAINING_DIR[i];
+				if (!classDir.isDirectory()) {
+					String msg = "Could not find training directory="
+							+ classDir
+							+ "\nHave you unpacked 4 newsgroups?";
+					System.out.println(msg); // in case exception gets lost in shell
+					throw new IllegalArgumentException(msg);
+				}
+
+				String[] trainingFiles = classDir.list();
+				for (int j = 0; j < trainingFiles.length; ++j) {
+					File file = new File(classDir,trainingFiles[j]);
+					String text = Files.readFromFile(file,"ISO-8859-1");
+					System.out.println("Training on " + CATEGORIES[i] + "/" + trainingFiles[j]);
+					Classification classification = new Classification(CATEGORIES[i]);
+					Classified<CharSequence> classified = new Classified<CharSequence>(text,classification);
+					classifier.handle(classified);
+				}
+
+				DataOutputStream outputStream;
+				outputStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(new File(CATEGORIES[i]+".model"))));
+				classifier.languageModel(CATEGORIES[i]).writeTo(outputStream);
 			}
 
-			String[] trainingFiles = classDir.list();
-			for (int j = 0; j < trainingFiles.length; ++j) {
-				File file = new File(classDir,trainingFiles[j]);
-				String text = Files.readFromFile(file,"ISO-8859-1");
-				System.out.println("Training on " + CATEGORIES[i] + "/" + trainingFiles[j]);
-				Classification classification
-						= new Classification(CATEGORIES[i]);
-				Classified<CharSequence> classified
-						= new Classified<CharSequence>(text,classification);
-				classifier.handle(classified);
-			}
 		}
-		//compiling
-		System.out.println("Compiling");
-		@SuppressWarnings("unchecked") // we created object so know it's safe
-				JointClassifier<CharSequence> compiledClassifier
-				= (JointClassifier<CharSequence>)
-				AbstractExternalizable.compile(classifier);
+		else
+		if (args[0].equals("classify"))
+		{
+			TESTING_DIR = new File(args[1]);
 
-		boolean storeCategories = true;
-		JointClassifierEvaluator<CharSequence> evaluator
-				= new JointClassifierEvaluator<CharSequence>(compiledClassifier,
-				CATEGORIES,
-				storeCategories);
-		for(int i = 0; i < CATEGORIES.length; ++i) {
-			File classDir = new File(TESTING_DIR,CATEGORIES[i]);
-			String[] testingFiles = classDir.list();
-			for (int j=0; j<testingFiles.length;  ++j) {
-				String text
-						= Files
-						.readFromFile(new File(classDir,testingFiles[j]),"ISO-8859-1");
-				System.out.print("Testing on " + CATEGORIES[i] + "/" + testingFiles[j] + " ");
-				Classification classification
-						= new Classification(CATEGORIES[i]);
-				Classified<CharSequence> classified
-						= new Classified<CharSequence>(text,classification);
-				evaluator.handle(classified);
-				JointClassification jc =
-						compiledClassifier.classify(text);
-				String bestCategory = jc.bestCategory();
-				String details = jc.toString();
-				System.out.println("Got best category of: " + bestCategory);
-				System.out.println(jc.toString());
-				System.out.println("---------------");
+			//classifier.languageModel(CATEGORIES[0]).
+			//compiling
+			System.out.println("Compiling");
+			@SuppressWarnings("unchecked") // we created object so know it's safe
+					JointClassifier<CharSequence> compiledClassifier
+					= (JointClassifier<CharSequence>) AbstractExternalizable.compile(classifier);
+
+			boolean storeCategories = true;
+			JointClassifierEvaluator<CharSequence> evaluator
+					= new JointClassifierEvaluator<CharSequence>(compiledClassifier,
+					CATEGORIES,
+					storeCategories);
+			for(int i = 0; i < CATEGORIES.length; ++i) {
+				File classDir = new File(TESTING_DIR, CATEGORIES[i]);
+				String[] testingFiles = classDir.list();
+				for (int j = 0; j < testingFiles.length; ++j) {
+					String text = Files.readFromFile(new File(classDir, testingFiles[j]), "ISO-8859-1");
+					System.out.print("Testing on " + CATEGORIES[i] + "/" + testingFiles[j] + " ");
+					Classification classification = new Classification(CATEGORIES[i]);
+					Classified<CharSequence> classified = new Classified<CharSequence>(text, classification);
+					evaluator.handle(classified);
+					JointClassification jc = compiledClassifier.classify(text);
+					String bestCategory = jc.bestCategory();
+					String details = jc.toString();
+					System.out.println("Got best category of: " + bestCategory);
+					System.out.println(jc.toString());
+					System.out.println("---------------");
+				}
 			}
+
+			ConfusionMatrix confMatrix = evaluator.confusionMatrix();
+			System.out.println("Total Accuracy: " + confMatrix.totalAccuracy());
+
+			System.out.println("\nFULL EVAL");
+			System.out.println(evaluator);
 		}
 
-		ConfusionMatrix confMatrix = evaluator.confusionMatrix();
-		System.out.println("Total Accuracy: " + confMatrix.totalAccuracy());
 
-		System.out.println("\nFULL EVAL");
-		System.out.println(evaluator);
+
 	}
 }
